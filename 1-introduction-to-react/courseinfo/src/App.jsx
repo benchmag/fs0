d4627511fd78032e608807f739f0018fd523d479
bench <ben.cheetham94@gmail.com>
2025-08-26 14:11:15 +1000

    Update App.jsx
    Completed exercises 1.1 and 1.2

--- 1-introduction-to-react/courseinfo/src/App.jsx
+++ 1-introduction-to-react/courseinfo/src/App.jsx
@@ -1,3 +1,35 @@
+const Header = (course) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (info) => {
+  return (
+    <div>
+      <Part data = {[info.p1, info.e2]}/>
+      <Part data = {[info.p2, info.e2]}/> 
+      <Part data = {[info.p3, info.e3]}/>
+    </div>
+  )
+}
+
+const Part = (info) => {
+  return (
+    <div>
+      <p> {info.data[0]}: {info.data[1]} </p>
+    </div>
+  )
+}
+
+const Total = (info) => {
+  return (
+    <p>Total number of exercises: {info.e1 + info.e2 + info.e3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +41,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name = {course}/>
+      <Content p1 = {part1} e1 = {exercises1} p2 = {part2} e2 = {exercises2} p3 = {part3} e3 = {exercises3}/>
+      <Total e1 = {exercises1} e2 = {exercises2} e3 = {exercises3}/>
     </div>
   )
 }
