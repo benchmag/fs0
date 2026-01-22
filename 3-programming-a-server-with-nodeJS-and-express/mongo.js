fc665606bb8817326f17a010fc2d898cd16cc686
bench <bcheeth94@proton.me>
2026-01-22 14:45:48 +1100

    uibuild

--- 3-programming-a-server-with-nodeJS-and-express/mongo.js
+++ 3-programming-a-server-with-nodeJS-and-express/mongo.js
@@ -1,19 +1,43 @@
 const mongoose = require('mongoose')
 
+//console.log('process.argv:', process.argv.length)
+
+if (process.argv.length === 3) {
+  const password = process.argv[2]
+  const url = `mongodb+srv://bcheeth94_db_user:${password}@cluster0.dqimbci.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
+   
+  //console.log('connecting to', url)
+  mongoose.set('strictQuery',false)
+  mongoose.connect(url, { family: 4 })
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  const Person = mongoose.model('Person', personSchema)
+  
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
+      mongoose.connection.close()      
+    })
+
+  })
+  return
+}
+
 if (process.argv.length < 5) {
   console.log('give password, name, and number as arguments')
   process.exit(1)
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
-//const url = `mongodb+srv://bcheeth94_db_user:${password}@cluster0.dqimbci.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
 const url = `mongodb+srv://bcheeth94_db_user:${password}@cluster0.dqimbci.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
-
 mongoose.connect(url, { family: 4 })
 
 // const noteSchema = new mongoose.Schema({
@@ -49,11 +73,11 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  name: name,
-  number: number,
+  name: newName,
+  number: newNumber,
 })
 
 person.save().then(result => {
-    console.log(`added ${name} number ${number} to phonebook`)
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
     mongoose.connection.close()
 })
