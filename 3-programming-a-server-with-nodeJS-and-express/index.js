6455db1db5d8809575ac982eb98222f13999b563
bench <bcheeth94@proton.me>
2026-01-28 10:50:21 +1100

    uibuild

--- 3-programming-a-server-with-nodeJS-and-express/index.js
+++ 3-programming-a-server-with-nodeJS-and-express/index.js
@@ -99,20 +99,26 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = request.params.id
+//   const note = notes.find(note => note.id === id)
 
-  if (note) {
-    response.status(200)
+//   if (note) {
+//     response.status(200)
+//     response.json(note)
+//   } else {
+//     response.status(404)
+//     response.send(
+//       `<h1>404 Not Found</h1>
+//       <p>Do a proper request</p>`
+//     )
+//   }
+// })
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404)
-    response.send(
-      `<h1>404 Not Found</h1>
-      <p>Do a proper request</p>`
-    )
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -129,25 +135,42 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body
+
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     id: generateId(),
+//   }
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+// }) 
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
-}) 
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
 app.put('/api/notes/:id', (request, response) => {
   const id = request.params.id
