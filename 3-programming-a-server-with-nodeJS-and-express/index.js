ba5ab59cbabd224bb91f19c2b7fd75a07900b4ef
bench <bcheeth94@proton.me>
2026-01-30 11:40:51 +1100

    uibuild

--- 3-programming-a-server-with-nodeJS-and-express/index.js
+++ 3-programming-a-server-with-nodeJS-and-express/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const morgan = require('morgan')
-//const cors = require('cors')
 
 //MIDDLEWARE
 //morgan for logging HTTP requests
@@ -16,55 +15,22 @@ app.use(express.json()) //parse JSON request bodies
 app.use(express.static('dist/phonebook')) //serve static files from the 'dist' directory
 
 //custom middleware to log request details
-// const requestLogger = (request, response, next) => {
-//   console.log('IP:', request.ip)
-//   //console.log('Path:  ', request.path)
-//   //console.log('Body:  ', request.body)
-//   //console.log('---')
-//   next()
-// }
+const requestLogger = (request, response, next) => {
+  console.log('IP:', request.ip)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 // app.use(requestLogger)
-
-//app.use(cors({ origin: 'http://localhost:5173' }))
-
-//custom middleware to handle unknown endpoints
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' })
-// }
-//app.use(unknownEndpoint)
 
 //MONGOOSE SETUP
 require('dotenv').config()
 //const Note = require('./models/note')
 const Person = require('./models/person')
 
-// const mongoose = require('mongoose')
-
-// // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-// const password = process.argv[2]
-// const url = `mongodb+srv://bcheeth94_db_user:${password}@cluster0.dqimbci.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-// mongoose.set('strictQuery',false)
-// mongoose.connect(url, { family: 4 })
-
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// })
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
-
-// const Note = mongoose.model('Note', noteSchema)
-
 
 //HOME ENDPOINT
-
 app.get('/', (request, response) => {
   response.status(200)
   response.send('<h1>Hello World!</h1>')
@@ -74,98 +40,32 @@ app.get('/notes', (request, response) => {
   // Served by express.static middleware above
 })
 
+//NOTES API ENDPOINTS//
+//..............//
+
+//GET
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
-
-// //NOTES API ENDPOINTS
-// let notes = [
-//   {
-//     id: "1",
-//     content: "HTML is easy",
-//     important: true
-//   },
-//   {
-//     id: "2",
-//     content: "Browser can execute only JavaScript",
-//     important: false
-//   },
-//   {
-//     id: "3",
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true
-//   }
-// ]
-
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-
-// app.get('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   const note = notes.find(note => note.id === id)
-
-//   if (note) {
-//     response.status(200)
-//     response.json(note)
-//   } else {
-//     response.status(404)
-//     response.send(
-//       `<h1>404 Not Found</h1>
-//       <p>Do a proper request</p>`
-//     )
-//   }
-// })
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
-
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
-
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// }) 
-
+//POST
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -183,6 +83,7 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+//PUT
 app.put('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const body = request.body
@@ -196,47 +97,46 @@ app.put('/api/notes/:id', (request, response) => {
   response.json(note)
 })
 
-//PERSONS API ENDPOINTS
-app.get('/api/persons', (request, response) => {
-  console.log(request.ip)
-  response.json(persons)
-})
+//PATCH
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  // if (person) {
-  //   response.json(person)
-  // } else {
-  //   response.status(404).end()
-  // }
-
-  if (person) {
-    response.send(
-      `<h1>${person.name}:</h1>
-      <p>${person.number}</p>`
-    )
-  } else {
-    response.status(404)
-    response.send(
-      `<h1>404 Not Found</h1>
-      <p>Do a proper request</p>`
-    )
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-
-  Person.findByIdAndDelete(id)
-    .then(() => {
+//DELETE
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
       response.status(204).end()
     })
     .catch(error => {
-      response.status(500).json({ error: 'Failed to delete person' })
+      console.log(error)
+      next(error)
     })
 })
 
+//PERSONS API ENDPOINTS
+//..............//
+
+//GET
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
+  })
+
+//POST
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -246,38 +146,45 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  //Check for unique name and number
-  Person.findOne({ name: body.name })
-    .then(existingPerson => {
-      if (existingPerson) {
-        return response.status(400).json({
-          error: 'name must be unique',
-          code: 'name-duplicate'
-        })
-      }
-      Person.findOne({ number: body.number })
-        .then(existingNumber => {
-          if (existingNumber) {
-            return response.status(400).json({
-              error: 'number must be unique',
-              code: 'number-duplicate'
-            })
-          }
-          const person = new Person({
-            name: body.name,
-            number: body.number,
-          })
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-          person.save().then(savedPerson => {
-            response.json(savedPerson)
-          })
-        })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+//PUT
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
+    .catch(error => next(error))
+  })
+
+//PATCH
+
+//DELETE
+app.delete('/api/persons/:id', (request, response, next) => {
+  console.log(`deleting id ${request.params.id}`)
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 //INFO ENDPOINTS
 app.get('/info', (request, response) => {
-  const phonebookLength = persons.length
+  const phonebookLength = Person.length
   const date = new Date()
 
   response.send(
@@ -286,6 +193,23 @@ app.get('/info', (request, response) => {
   )  
 })
 
+//custom middleware to handle unknown endpoints
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+//app.use(unknownEndpoint)
+
+//custom error handling middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
